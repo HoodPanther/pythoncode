7710706ea283517f43b7e2acca22b5087b435783
kbporter <kbporter@users.noreply.github.com>
2016-02-20 22:48:59 -0500

    added pos/neg graphs

--- tweetExplore_fns.py
+++ tweetExplore_fns.py
@@ -26,6 +26,33 @@ def extract_tweets_fieldOfInterest(data, interest_field):
 				tweet_list.append(curtweet)
 	return tweet_list
 
+def makeWordcloud(tweetList):
+    """ Joins tweet list into single string, removes stop words and RT, then generates word cloud """
+    from wordcloud import WordCloud, STOPWORDS
+    newSTOP = STOPWORDS
+    newlist = []
+    for itweet,val in enumerate(tweetList):
+        curtweet = tweetList[itweet]
+        curtweet.replace('RT','')
+        newlist.append(curtweet)
+        
+    # add stopwords to exclude from wordcloud - 
+#     newSTOP.update(('co','https','t','\'RT','R','T','-RT','RT@'))
+    joinedTexts = " ".join(str(x) for x in newlist)
+    setattr(WordCloud,"setfont",'/anaconda/envs/eq_env/lib/python3.5/site-packages/matplotlib/mpl-data/fonts/ttf/cmmi10.ttf')
+    # font_path='/anaconda/envs/eq_env/lib/python3.5/site-packages/matplotlib/mpl-data/fonts/ttf/cmmi10.ttf',
+
+    #generate wordcloud
+    wordcloud = WordCloud(stopwords=newSTOP,
+                              background_color='black',
+                              width=1700,
+                              height=1400
+                             ).generate(joinedTexts)
+    # display wordcloud
+#     plt.imshow(wordcloud)
+#     plt.axis('off')
+#     plt.show();
+
 
 
 def extract_indices(data, interest_field):
